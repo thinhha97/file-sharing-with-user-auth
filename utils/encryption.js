4183a7de94a1a1cfe86852d63a80988aa76eee8b
Thinh Ha <havanthinh97@gmail.com>
2022-07-02 23:12:19 +0700

    add encryption and decryption.

---
+++ utils/encryption.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto')
+
+const algorithm = process.env.ENCRYPTION_ALGORITHM
+const secretKey = process.env.ENCRYPTION_SECRET_KEY
+
+const encrypt = (text) => {
+  const iv = crypto.randomBytes(16)
+
+  const cipher = crypto.createCipheriv(algorithm, secretKey, iv)
+
+  const encrypted = Buffer.concat([cipher.update(text), cipher.final()])
+
+  return {
+    iv: iv.toString('hex'),
+    content: encrypted.toString('hex'),
+  }
+}
+
+const decrypt = (hash) => {
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    secretKey,
+    Buffer.from(hash.iv, 'hex')
+  )
+
+  const decrpyted = Buffer.concat([
+    decipher.update(Buffer.from(hash.content, 'hex')),
+    decipher.final(),
+  ])
+
+  return decrpyted.toString()
+}
+
+module.exports = {
+  encrypt,
+  decrypt,
+}
