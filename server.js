c220c73b69118a49c10dfbcd8007448a71f06a6e
Thinh Ha <havanthinh97@gmail.com>
2022-07-02 23:09:20 +0700

    user cookie-parser.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const route = require('./routes')
 const path = require('path')
-
+const cookieParser = require('cookie-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'node_modules', 'bootstrap')))
+app.use(cookieParser())
 app.use(route)
 
 module.exports = app
